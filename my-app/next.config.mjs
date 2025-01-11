27c16c617e6649d021dc6b181384f41f22e5cfd9
naghmana1220 <naghmanaasif85@gmail.com>
2025-01-11 18:05:18 +0500

    Error Resolved

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io'], // Add Sanity's CDN domain
+    },
+  };
+  
+  export default nextConfig;
+  
